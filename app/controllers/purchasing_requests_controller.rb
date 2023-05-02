340842ea1539d8d9b4ec20358fa3029aab27b4be
Mathew Hocking <matt8443478@gmail.com>
2023-05-02 20:27:09 +1000

    added the purchase controller

--- app/controllers/purchasing_requests_controller.rb
+++ app/controllers/purchasing_requests_controller.rb
@@ -1,2 +1,50 @@
 class PurchasingRequestsController < ApplicationController
+  before_action :set_purchasing_request, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @purchasing_requests = PurchasingRequest.all
+  end
+
+  def show
+  end
+
+  def new
+    @purchasing_request = current_user.purchasing_requests.build
+  end
+
+  def create
+    @purchasing_request = PurchasingRequest.new(purchasing_request_params)
+
+    if @purchasing_request.save
+      redirect_to @purchasing_request
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @purchasing_request.update(purchasing_request_params)
+      redirect_to @purchasing_request
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    purchasing_request.destroy
+    redirect_to purchasing_requests_path
+  end
+
+  private
+
+  def purchasing_request_params
+    params.require(:purchasing_request).permit(:pr_quantity, :delivery_date, :delivery_time_slot, :user_id, :supplier_id)
+  end
+
+  def set_purchasing_request
+    @purchasing_request = PurchasingRequest.find(params[:id])
+  end
 end
