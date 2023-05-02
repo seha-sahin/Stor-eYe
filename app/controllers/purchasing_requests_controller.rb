6fc71923648dcd5ffc41d57ffcae73642558ac0d
Mathew Hocking <matt8443478@gmail.com>
2023-05-02 22:20:48 +1000

    purchase requst is now working

--- app/controllers/purchasing_requests_controller.rb
+++ app/controllers/purchasing_requests_controller.rb
@@ -10,38 +10,42 @@ class PurchasingRequestsController < ApplicationController
 
   def new
     @purchasing_request = current_user.purchasing_requests.build
+    @supplier = @purchasing_request.build_supplier
   end
 
   def create
     @purchasing_request = PurchasingRequest.new(purchasing_request_params)
+    @purchasing_request.user = current_user
+    @purchasing_request.supplier = Supplier.find(params[:purchasing_request][:supplier_id])
 
     if @purchasing_request.save
-      redirect_to @purchasing_request
+      redirect_to purchasing_requests_path, notice: 'Purchasing request was successfully created.'
     else
       render :new, status: :unprocessable_entity
     end
   end
+
 
   def edit
   end
 
   def update
     if @purchasing_request.update(purchasing_request_params)
-      redirect_to @purchasing_request
+      redirect_to purchasing_requests_path, notice: 'Purchasing request was successfully updated.'
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
-    purchasing_request.destroy
-    redirect_to purchasing_requests_path
+    @purchasing_request.destroy
+    redirect_to purchasing_requests_path, notice: 'Purchasing request was successfully destroyed.'
   end
 
   private
 
   def purchasing_request_params
-    params.require(:purchasing_request).permit(:pr_quantity, :delivery_date, :delivery_time_slot, :user_id, :supplier_id)
+    params.require(:purchasing_request).permit(:pr_quantity, :delivery_date, :delivery_time_slot, :supplier_id, supplier_attributes: [:name, :phone_number, :email])
   end
 
   def set_purchasing_request
