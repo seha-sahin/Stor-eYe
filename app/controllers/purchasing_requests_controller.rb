d52b415fe1bc26671f956d3ac1bc91531171897d
Mathew Hocking <matt8443478@gmail.com>
2023-05-08 15:49:11 +1000

    PR form working

--- app/controllers/purchasing_requests_controller.rb
+++ app/controllers/purchasing_requests_controller.rb
@@ -1,40 +1,34 @@
 class PurchasingRequestsController < ApplicationController
   before_action :set_purchasing_request, only: [:show, :edit, :update, :destroy]
 
+  before_action :set_purchasing_request, only: [:show, :edit, :update, :destroy]
+
   def index
     @purchasing_requests = PurchasingRequest.all
-    @wines_by_supplier = Wine.includes(:supplier).all.group_by { |wine| wine.supplier.id }
   end
 
   def show
   end
 
   def new
-    @purchasing_request = PurchasingRequest.new(supplier_id: params[:supplier_id])
+    @purchasing_request = PurchasingRequest.new
     @suppliers = Supplier.all.includes(:wines)
-    @wines_by_supplier = @suppliers.each_with_object({}) do |supplier, obj|
-      obj[supplier.id] = supplier.wines.order(maker: :asc)
+    @suppliers.each do |supplier|
+      supplier.wines.each do |wine|
+        @purchasing_request.purchasing_request_items.build(wine: wine, quantity: 0)
+      end
     end
-    @purchasing_request.purchasing_request_items.build
   end
 
   def create
     @purchasing_request = PurchasingRequest.new(purchasing_request_params)
     @purchasing_request.user = current_user
-    supplier_id = params[:purchasing_request][:supplier_id]
-    @purchasing_request.supplier = Supplier.find_by(id: supplier_id)
-
-    if @purchasing_request.supplier.nil?
-      flash[:alert] = "Supplier with ID #{supplier_id} could not be found"
-      render :new, status: :unprocessable_entity
-    elsif @purchasing_request.save
-      redirect_to purchasing_requests_path, notice: 'Purchasing request was successfully created.'
+    if @purchasing_request.save
+      redirect_to @purchasing_request
     else
-      flash[:alert] = 'There was an error creating the purchasing request.'
       render :new, status: :unprocessable_entity
     end
   end
-
 
   def edit
   end
@@ -52,16 +46,8 @@ class PurchasingRequestsController < ApplicationController
     redirect_to purchasing_requests_path, notice: 'Purchasing request was successfully destroyed.'
   end
 
-  private
-
   def purchasing_request_params
-    params.require(:purchasing_request).permit(
-      :delivery_date,
-      :delivery_time_slot,
-      :supplier_id,
-      supplier_attributes: [:name, :phone_number, :email],
-      purchasing_request_items_attributes: [:id, :quantity, :wine_id, :_destroy]
-    )
+    params.require(:purchasing_request).permit(:supplier_id, :delivery_date, :delivery_time_slot, purchasing_request_items_attributes: [:id, :wine_id, :quantity])
   end
 
   def set_purchasing_request
--- app/models/purchasing_request.rb
+++ app/models/purchasing_request.rb
@@ -8,6 +8,15 @@ class PurchasingRequest < ApplicationRecord
   has_many :purchasing_request_items, dependent: :destroy
   has_many :items, through: :purchasing_request_items
   has_many :wines, through: :purchasing_request_items
+  accepts_nested_attributes_for :purchasing_request_items
+
+  TIME_SLOTS = [
+    ['8:00 AM - 10:00 AM', '8:00-10:00'],
+    ['10:00 AM - 12:00 PM', '10:00-12:00'],
+    ['12:00 PM - 2:00 PM', '12:00-14:00'],
+    ['2:00 PM - 4:00 PM', '14:00-16:00'],
+    ['4:00 PM - 6:00 PM', '16:00-18:00']
+  ].freeze
 
   def total_quantity
     purchasing_request_items.sum(:quantity)
