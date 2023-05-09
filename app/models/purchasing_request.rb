d1d8f9773e61e4f8988aac5a6b821c90ed93b633
Mathew Hocking <matt8443478@gmail.com>
2023-05-09 21:09:15 +1000

    bug fixes

--- app/models/purchasing_request.rb
+++ app/models/purchasing_request.rb
@@ -22,16 +22,4 @@ class PurchasingRequest < ApplicationRecord
     purchasing_request_items.sum(:quantity)
   end
 
-  pg_search_scope :search_by_pr_number,
-    against: :pr_number,
-    using: { tsearch: { prefix: true } }
-
-  pg_search_scope :search_by_pr_quantity,
-    against: { pr_quantity: 'A' },
-    using: { tsearch: { prefix: true } },
-    associated_against: {
-      supplier: { name: 'A' },
-      user: { email: 'A' }
-    }
-
 end
