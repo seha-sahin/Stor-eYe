9b6384b400362f96cc5df6b7883947f1f84442e9
Mathew Hocking <matt8443478@gmail.com>
2023-05-09 19:28:42 +1000

    removed JS

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -5,7 +5,6 @@
 import { application } from "./application"
 
 import HelloController from "./hello_controller"
-import PurchasingRequestFormController from "./purchasing_request_form_controller"
+
 
 application.register("hello", HelloController)
-application.register("purchasing-request-form", PurchasingRequestFormController)
