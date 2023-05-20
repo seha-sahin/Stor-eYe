f306f2e7a1f705421d747eec49aa27016b60cf0d
Seha Sahin <sahinseha@gmail.com>
2023-05-20 18:59:00 +1000

    add map to home, wines index, wines show

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,6 +7,9 @@ import { application } from "./application"
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
+import HomepagemapController from "./homepagemap_controller"
+application.register("homepagemap", HomepagemapController)
+
 import MapController from "./map_controller"
 application.register("map", MapController)
 
