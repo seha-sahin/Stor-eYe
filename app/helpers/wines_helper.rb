e1c049a193d911e2adba7b6b225decff5bcf4013
Romain H <119086709+Romain852@users.noreply.github.com>
2023-05-22 18:13:42 +0800

    Update wines_helper.rb

--- app/helpers/wines_helper.rb
+++ app/helpers/wines_helper.rb
@@ -1,15 +1,1 @@
-module WinesHelper
-  def sortable(column, title = nil)
-    title ||= column.titleize
-    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-    link_to title, {sort: column, direction: direction}
-  end
 
-  def sort_column
-    Wine.column_names.include?(params[:sort]) ? params[:sort] : "vintage"
-  end
-
-  def sort_direction
-    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
-  end
-end
