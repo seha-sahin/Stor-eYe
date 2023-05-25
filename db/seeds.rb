5920b4a6848882add94db86c374354e0ee78063f
Mathew Hocking <matt8443478@gmail.com>
2023-05-25 21:04:18 +1000

    updated the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
 require "faker"
 
-# CLEAN UP DATABASE
-puts "Deleting all wines"
-Wine.destroy_all
+# # CLEAN UP DATABASE
+# puts "Deleting all wines"
+# Wine.destroy_all
 
-puts "Deleting all storages"
-StorageLocation.destroy_all
+# puts "Deleting all storages"
+# StorageLocation.destroy_all
 
-puts "Deleting all restaurants"
-Restaurant.destroy_all
+# puts "Deleting all restaurants"
+# Restaurant.destroy_all
 
-puts "Deleting all suppliers"
-Supplier.destroy_all
+# puts "Deleting all suppliers"
+# Supplier.destroy_all
 
 # SEED RESTAURANTS
 3.times do
